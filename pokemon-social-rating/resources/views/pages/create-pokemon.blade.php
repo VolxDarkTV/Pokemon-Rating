f67f12414779996d10efbc6674f3f930f763d705
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-04 13:34:00 +0100

    fix create-pokemon style

--- pokemon-social-rating/resources/views/pages/create-pokemon.blade.php
+++ pokemon-social-rating/resources/views/pages/create-pokemon.blade.php
@@ -1,60 +1,69 @@
 @extends('layouts/main-layouts')
 
 @section('main')
-    <section>
+    <section class="container">
 
-        <h1>CREATE NEW SAINT</h1>
-        <form method="POST" action="{{ route('pokemonStore') }}">
+        <h4>Create YOUR Pokemon</h4>
+        <form class="d-flex flex-column align-items-center gap-2" method="POST" action="{{ route('pokemonStore') }}">
             @csrf
+
+            <div>
+                {{-- pokedex_n --}}
+                <input type="number" name="pokedex_n" id="pokedex_n" placeholder="Pokedex number">
+                {{-- <label for="pokedex_n">Pokedex number</label> --}}
+
+                {{-- name --}}
+                <input type="text" name="name" id="name" placeholder="Name">
+                {{-- <label for="name">Name</label> --}}
+            </div>
             
-            {{-- pokedex_n --}}
-            <input type="number" name="pokedex_n" id="pokedex_n">
-            <label for="pokedex_n">pokedex_n</label>
-            <br>
-            {{-- img_link --}}
-            <input type="url" name="img_link" id="img_link">
-            <label for="img_link">img_link</label>
-            <br>
-            {{-- name --}}
-            <input type="text" name="name" id="name">
-            <label for="name">name</label>
-            <br>
-            {{-- sex --}}
-            <input type="text" name="sex" id="sex">
-            <label for="sex">sex</label>
-            <br>
-            {{-- type --}}
-            <input type="text" name="type" id="type">
-            <label for="type">type</label>
-            <br>
-            {{-- type_weakness --}}
-            <input type="text" name="type_weakness" id="type_weakness">
-            <label for="type_weakness">type_weakness</label>
-            <br>
-            {{-- height --}}
-            <input type="text" name="height" id="height">
-            <label for="height">height</label>
-            <br>
-            {{-- weight --}}
-            <input type="text" name="weight" id="weight">
-            <label for="weight">weight</label>
-            <br>
+            <div>
+                {{-- img_link --}}
+                <input type="url" name="img_link" id="img_link" placeholder="Link Image">
+                {{-- <label for="img_link">Link image</label> --}}
+                {{-- sex --}}
+                <input type="text" name="sex" id="sex" placeholder="Pokemon Sex">
+                {{-- <label for="sex">Sex</label> --}}
+            </div>
+
+            <div>
+                
+                {{-- type --}}
+                <input type="text" name="type" id="type" placeholder="Type">
+                {{-- <label for="type">Type</label> --}}
+                
+                {{-- type_weakness --}}
+                <input type="text" name="type_weakness" id="type_weakness" placeholder="Type Weakness">
+                {{-- <label for="type_weakness">Type weakness</label> --}}
+                
+                {{-- height --}}
+                <input type="text" name="height" id="height" placeholder="Height">
+                {{-- <label for="height">Height</label> --}}
+                
+                {{-- weight --}}
+                <input type="text" name="weight" id="weight" placeholder="Weight">
+                {{-- <label for="weight">Weight</label> --}}
+            </div>
+            
+            
+            
             {{-- ability --}}
-            <input type="text" name="ability" id="ability">
-            <label for="ability">ability</label>
-            <br>
+            <input type="text" name="ability" id="ability" placeholder="Ability">
+            {{-- <label for="ability">Ability</label> --}}
+            
             {{-- category --}}
-            <input type="text" name="category" id="category">
-            <label for="category">category</label>
-            <br>
+            <input type="text" name="category" id="category" placeholder="Category">
+            {{-- <label for="category">Category</label> --}}
+            
             {{-- lv --}}
-            <input type="number" name="lv" id="lv">
-            <label for="lv">lv</label>
-            <br>
+            <input type="number" name="lv" id="lv" placeholder="Lv">
+            {{-- <label for="lv">Lv</label> --}}
+            
             {{-- description --}}
-            <input type="text" name="description" id="description">
-            <label for="description">description</label>
-            <br>
+            {{-- <input type="text" name="description" id="description" placeholder="Description"> --}}
+            {{-- <label for="description">Description</label> --}}
+            <h5>Description</h5>
+            <textarea name="description" id="" cols="30" rows="2" placeholder="Describe your Pokemon"></textarea>
             <input type="submit" value="Crea il TUO Pokemon">
         </form>
     </section>
